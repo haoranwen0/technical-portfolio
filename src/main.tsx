5f7770d04584ef49cc6acd366f22b2375cec791e
Haoran Wen <hran.wen@gmail.com>
2026-04-29 09:48:33 -0400

    package: vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.tsx";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <>
+    <Analytics />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </>,
 );
